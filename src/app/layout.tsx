2d3f48d58047dd825e2b0e11d88f815d0d04c828
bachorz.maciej@gmail.com <bachorz.maciej@gmail.com>
2026-06-11 23:36:26 +0200

    Add OpenGraph processing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
     siteName: "FutureFactoryX",
     images: [
       {
-        url: "/og-image.png",
+        url: "/og/futurefactoryx-og.png",
         width: 1200,
         height: 630,
         alt: "FutureFactoryX",
